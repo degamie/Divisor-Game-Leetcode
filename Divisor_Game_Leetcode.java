bd918479f0892d953e367b04a4a693788f02f35d
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-02-07 09:57:52 +0530

    Create Divisor_Game_Leetcode.java

---
+++ Divisor_Game_Leetcode.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean divisorGame(int n) {
+      //T(C(N)) S(C(N)) and  ==O(1) as it requires constant Time complexity in constant space
+        //Optimized Solution
+        return n%2==0;
+    }
+}
